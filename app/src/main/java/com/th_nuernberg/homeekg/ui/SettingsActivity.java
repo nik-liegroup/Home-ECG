280f7de92f3bdd01c22d494b7c09ed31d6a39cc7
Nik <nikfirebaseth@gmail.com>
2021-02-15 03:32:11 +0100

    version 1.1.5 Settings Tab updated

--- app/src/main/java/com/th_nuernberg/homeekg/ui/SettingsActivity.java
+++ app/src/main/java/com/th_nuernberg/homeekg/ui/SettingsActivity.java
@@ -1,14 +1,40 @@
 package com.th_nuernberg.homeekg.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Patterns;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.th_nuernberg.homeekg.R;
 
+import java.util.Set;
+
 public class SettingsActivity extends AppCompatActivity {
+
+    private Button reset_password, delete_account, logout_account;
+    private FirebaseUser user;
+    private DatabaseReference reference_database;
+    private String userID;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +44,83 @@ public class SettingsActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Settings");
+
+        //Authentication
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference_database = FirebaseDatabase.getInstance().getReference("users");
+        userID = user.getUid();
+
+        reset_password = (Button) findViewById(R.id.resetPassword);
+        delete_account = (Button) findViewById(R.id.deleteAccount);
+        logout_account = (Button) findViewById(R.id.logoutAccount);
+
+        logout_account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(SettingsActivity.this);
+                builder.setPositiveButton(R.string.logout_ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Intent intent = new Intent(SettingsActivity.this, LoginActivity.class)
+                                .putExtra("finish", true)
+                                .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP |
+                                        Intent.FLAG_ACTIVITY_CLEAR_TASK |
+                                        Intent.FLAG_ACTIVITY_NEW_TASK);
+                        FirebaseAuth.getInstance().signOut();
+                        startActivity(intent);
+                        finish();
+                    }
+                });
+                builder.setNegativeButton(R.string.logout_cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                    }
+                });
+
+                builder.setMessage(R.string.logout_dialog_message)
+                        .setTitle(R.string.logout_dialog_title);
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+
+        delete_account.setOnClickListener(new View.OnClickListener() {
+            //TODO
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(SettingsActivity.this);
+                builder.setPositiveButton(R.string.logout_ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Intent intent = new Intent(SettingsActivity.this, LoginActivity.class)
+                                .putExtra("finish", true)
+                                .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP |
+                                        Intent.FLAG_ACTIVITY_CLEAR_TASK |
+                                        Intent.FLAG_ACTIVITY_NEW_TASK);
+                        FirebaseAuth.getInstance().signOut();
+                        Toast.makeText(SettingsActivity.this, "Account has been deleted successfully", Toast.LENGTH_SHORT).show();
+
+                        //TODO Delete Account Logic
+
+                        startActivity(intent);
+                        finish();
+                    }
+                });
+                builder.setNegativeButton(R.string.logout_cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                    }
+                });
+
+                builder.setMessage(R.string.delete_dialog_message)
+                        .setTitle(R.string.delete_dialog_title);
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+
+        reset_password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    //TODO RESET PASSWORD LOGIC
+                }
+        });
     }
 
     @Override
